f1d0a36b1121a4f4f770c50b5179e640610d28c5
Heeseon Cheon <heeseon0315@gmail.com>
2022-07-26 00:24:46 +0900

    feat: 14_longest_common_prefix 예외 처리 제외 결과 추가

--- problems/python/easy/14_longest_common_prefix.py
+++ problems/python/easy/14_longest_common_prefix.py
@@ -53,3 +53,9 @@ class Solution:
 Runtime: 69 ms, faster than 18.83% of Python3 online submissions for Longest Common Prefix.
 Memory Usage: 14 MB, less than 49.70% of Python3 online submissions for Longest Common Prefix.
 """
+
+"""
+## 위 함수에서 if not strs: return "" 부분 제외 시 결과
+Runtime: 41 ms, faster than 81.58% of Python3 online submissions for Longest Common Prefix.
+Memory Usage: 13.8 MB, less than 88.15% of Python3 online submissions for Longest Common Prefix.
+"""
