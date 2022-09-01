a326e2b80904258637290157eee436db6e54b106
Heeseon Cheon <heeseon0315@gmail.com>
2022-09-01 20:44:19 +0900

    Time: 47 ms (51.30%), Space: 13.8 MB (53.81%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,8 +1,15 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
+        # https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/discuss/812200/Python-one-liner-using-eval-function-(28ms-14MB)
+        return eval('*'.join(str(n))) - eval('+'.join(str(n)))        
+
+        """ my solution
+        ## Runtime: 65 ms, faster than 9.49% / Memory Usage: 13.9 MB, less than 53.81%
+        
         product, total = 1, 0
         for nn in str(n):
             product *= int(nn)
             total += int(nn)
         return product - total
-            
+        """
+ 
