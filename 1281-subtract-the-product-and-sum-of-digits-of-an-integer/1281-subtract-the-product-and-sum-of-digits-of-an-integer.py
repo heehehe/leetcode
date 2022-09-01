70af1584d93d5350334269f75a44d585e6176ef0
Heeseon Cheon <heeseon0315@gmail.com>
2022-09-01 20:46:46 +0900

    Time: 33 ms (89.83%), Space: 14 MB (9.70%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,8 +1,14 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
-        # https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/discuss/812200/Python-one-liner-using-eval-function-(28ms-14MB)
-        return eval('*'.join(str(n))) - eval('+'.join(str(n)))        
-
+        # https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/discuss/812200/Python-one-liner-using-eval-function-(28ms-14MB)/1563776
+        a = list(map(int, iter(str(n))))
+        return reduce(lambda x,y: x*y, a) - reduce(lambda x,y: x+y, a)    
+        
+        """ https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/discuss/812200/Python-one-liner-using-eval-function-(28ms-14MB)
+        ## Runtime: 47 ms, faster than 51.30% / Memory Usage: 13.8 MB, less than 53.81% 
+        return eval('*'.join(str(n))) - eval('+'.join(str(n)))
+        """
+        
         """ my solution
         ## Runtime: 65 ms, faster than 9.49% / Memory Usage: 13.9 MB, less than 53.81%
         
